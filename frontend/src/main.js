8e85a01609985fa9495f7753a9fe1bf508cd2579
Maxycle <maxycle@hotmail.com>
2024-06-23 14:31:56 +0200

    fix: add library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,11 @@ import { createPinia } from 'pinia'
 import './assets/main.css'
 import { useSessionStore } from "@/stores/modules/sessionStore"
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
@@ -33,6 +35,13 @@ const loadAuthToken = async () => {
 }
 
 const vuetify = createVuetify({
+	icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
 	components,
 	directives
 })
