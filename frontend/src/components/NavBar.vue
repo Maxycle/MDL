02fb84eb71ed6cebaf34628d014c6988c91dc61f
Maxycle <maxycle@hotmail.com>
2024-06-27 00:50:10 +0200

    feat: add admin button

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,15 @@
 <template>
-	<div class="flex justify-between px-4 items-center bg-gradient-to-r from-neutral-400 to-neutral-300 shadow-lg shadow-neutral-700">
-		<div class="font-sus">Salut {{ loggedInUser.username }}, devient un vrai libertarien avec des couilles en fer !!</div>
+	<div
+		class="flex justify-between px-4 items-center bg-gradient-to-r from-neutral-400 to-neutral-300 shadow-lg shadow-neutral-700">
+		<div class="font-sus">Salut {{ loggedInUser.username }}, devient un vrai libertarien avec des couilles en fer !!
+		</div>
 		<div class="flex justify-end space-x-2">
 			<NavBarButton :isActive="isRouteActive('/')">
 				<span class="relative"><router-link to="/">Home</router-link></span>
+			</NavBarButton>
+			<NavBarButton v-if="loggedInUser.admin" :isActive="isRouteActive('/nouvelle-question')" @mouseover="showMenu" @mouseout="hideMenu" class="relative">
+				<span>Admin</span>
+				<AdminMenu v-show="showAdminMenu" class="absolute top-12 -left-10 w-fit" />
 			</NavBarButton>
 			<NavBarButton @click="redirectToEditProfile">
 				<span class="relative">Edit profile</span>
@@ -14,21 +20,23 @@
 			<NavBarButton v-else :isActive="isRouteActive('/Login')" @click="router.push('/Login')">
 				<span class="relative">Login</span>
 			</NavBarButton>
+
 		</div>
 	</div>
 </template>
 
 <script setup>
 import axios from 'axios';
-import NavBarButton from './buttons/NavBarButton.vue';
-import { useRoute, useRouter } from 'vue-router';
-import { ref, computed } from 'vue';
-import { useSessionStore } from '@/stores/modules/sessionStore';
+import NavBarButton from './buttons/NavBarButton.vue'
+import AdminMenu from './AdminMenu.vue'
+import { useRoute, useRouter } from 'vue-router'
+import { ref, computed } from 'vue'
+import { useSessionStore } from '@/stores/modules/sessionStore'
 
 const store = useSessionStore();
 const router = useRouter();
 const route = useRoute();
-const isLoading = ref(false)
+const showAdminMenu = ref(false)
 
 axios.defaults.baseURL = 'http://localhost:3000'
 
@@ -38,7 +46,7 @@ const loggedInUser = computed(() => {
 
 const isRouteActive = (path) => {
 	return route.path === path;
-};
+}
 
 const logout = async () => {
 	try {
@@ -47,9 +55,17 @@ const logout = async () => {
 	} catch (error) {
 		console.error('Error logging out', error);
 	}
-};
+}
+
+const showMenu = () => {
+	showAdminMenu.value = true
+}
+
+const hideMenu = () => {
+	showAdminMenu.value = false
+}
 
 const redirectToEditProfile = () => {
 	window.location.href = '/users/edit';
-};
+}
 </script>
