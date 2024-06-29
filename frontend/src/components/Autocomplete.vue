fc70b5eb9ef2bb1ffa0182afa72150b09fbbd017
Maxycle <maxycle@hotmail.com>
2024-06-29 19:09:46 +0200

    fix: correct key

--- frontend/src/components/Autocomplete.vue
+++ frontend/src/components/Autocomplete.vue
@@ -1,21 +1,11 @@
 <template>
 	<div>
 		<div class="relative">
-			<input
-				type="text"
-				id="autocomplete"
-				:placeholder="placeholder"
-				v-model="selectedItem"
-				@input="onInput"
-				class="py-2 px-4 flex justify-between items-center rounded w-full shadow-md shadow-black bg-neutral-300 focus:bg-green-200"
-			/>
-			<ul v-if="dropdownOptions.length" class="absolute z-10 mt-2 bg-white border rounded shadow-md w-full">
-				<li
-					v-for="item in dropdownOptions"
-					:key="item.content"
-					@click="selectItem(item.content)"
-					class="p-4 rounded hover:bg-blue-500"
-				>
+			<input type="text" id="autocomplete" :placeholder="placeholder" v-model="selectedItem" @input="onInput"
+				class="py-2 px-4 flex justify-between items-center rounded w-full shadow-md shadow-black bg-neutral-300 focus:bg-green-200" />
+			<ul v-if="filteredItems.length" class="absolute z-10 mt-2 bg-white border rounded shadow-md w-full">
+				<li v-for="(item, itemIndex) in filteredItems" :key="itemIndex" @click="selectItem(item.content)"
+					class="p-4 rounded hover:bg-blue-500">
 					<div>{{ item.content }}</div>
 				</li>
 			</ul>
@@ -25,7 +15,7 @@
 
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed } from 'vue'
 import { useSessionStore } from '@/stores/modules/sessionStore'
 
 const props = defineProps({
@@ -41,11 +31,7 @@ const filteredItems = ref([])
 const placeholder = ref('ta gueule')
 const selectionMade = ref(false)
 const urlToFetch = ref('/questions')
-
 const sessionStore = useSessionStore()
-
-const dropdownOptions = computed(() => filteredItems.value)
-
 
 const filterItems = () => {
 	selectionMade.value = false
