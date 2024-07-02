0c9e1de71b249faa928e6a0648d69db1bd434ae6
Maxycle <maxycle@hotmail.com>
2024-07-02 13:33:13 +0200

    fix: style

--- frontend/src/components/Hero.vue
+++ frontend/src/components/Hero.vue
@@ -2,7 +2,7 @@
 	<div class="relative bg-black flex items-center justify-center">
 		<img ref="imageRef" src="/img/Flag_of_Anarcho-capitalism.png" alt="yellow and black triangles"
 			class="absolute inset-0 h-full w-full object-fill md:object-center" />
-		<h1 v-for="(letter, index) in 'Mouvement&nbsp;des&nbsp;libertariens'" 
-		class="z-10 text-5xl font-extrabold text-orange-500 hover:text-green-500 hover:animate-spin transition duration-300" >{{ letter }}</h1>
+		<h1 v-for="(letter, index) in 'Mouvement&nbsp;des&nbsp;Libertariens'" 
+		class="z-10 text-8xl font-serif font-extrabold text-orange-600 hover:text-green-500 hover:animate-spin transition duration-300 " >{{ letter }}</h1>
 	</div>
 </template>
