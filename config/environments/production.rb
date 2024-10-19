e04cd45230cdfe8000fe5164c1be78d53699eaea
Maxycle <maxycle@hotmail.com>
2024-10-19 19:36:22 +0200

    feat: add smtp mail server to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,17 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "MDL_production"
-
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		address: "smtp.gmail.com",
+		port: 587,
+		domain: "libertosss.com",
+		user_name: ENV["GMAIL_USERNAME"],
+		password: ENV["GMAIL_PASSWORD"],
+		authentication: "plain",
+		enable_starttls_auto: true
+	}
+	
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
