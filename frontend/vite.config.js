b0abf648a6574dd2f72b5505d63988b31e0ed8f3
Maxycle <maxycle@hotmail.com>
2024-10-25 13:12:47 +0200

    fix: api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/': {
+      '/api': {
         target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',
         changeOrigin: true,
       }
