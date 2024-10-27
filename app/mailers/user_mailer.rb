dc1ad8b2605fe050fbf4082ed2cbeafbac81d727
Maxycle <maxycle@hotmail.com>
2024-10-27 18:11:41 +0100

    fix: adjust user_mailer for production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts = {})
     @token = token
-    @confirmation_link = "http://localhost:5173/confirmation?confirmation_token=#{token}"
+    @confirmation_link = "http://libertarien/api/confirmation?confirmation_token=#{token}"
     opts[:template_path] = 'devise/mailer' # Specify where to find the template
     super
   end
