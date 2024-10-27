31ea42cf4b4d5b0b14219463ffba1b44d438e94d
Maxycle <maxycle@hotmail.com>
2024-10-27 18:20:51 +0100

    fix: correct confirmation url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts = {})
     @token = token
-    @confirmation_link = "http://libertarien/confirmation?confirmation_token=#{token}"
+    @confirmation_link = "http://libertarien.net/confirmation?confirmation_token=#{token}"
     opts[:template_path] = 'devise/mailer' # Specify where to find the template
     super
   end
