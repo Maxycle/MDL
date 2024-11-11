3327a386a7be7e3ec2cf202cf249620a940b8273
Maxycle <maxycle@hotmail.com>
2024-11-11 19:34:49 +0100

    fix: adjust cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Update this to be more restrictive in production
+    origins 'http://www.libertarien.net' # Update this to be more restrictive in production
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
