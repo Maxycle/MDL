b88d747ca1b2d8a5174cd1af9fb15bec87782cb9
Maxycle <maxycle@hotmail.com>
2024-11-11 17:15:53 +0100

    fix: add confirmation frontend route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -21,7 +21,8 @@ const routes = [
 	{ path: '/utilisateurs', name: 'UsersScores', component: UsersScores, meta: { requiresAuth: false } },
 	{ path: '/upload-nouvelles-questions', name: 'UploadNewQuestions', component: UploadNewQuestions, meta: { requiresAuth: false } },
 	{ path: '/edit-profile', name: 'EditProfile', component: EditProfile, meta: { requiresAuth: false } },
-	{ path: '/email-confirmation', name: 'EmailConfirmation', component: EmailConfirmation }
+	{ path: '/email-confirmation', name: 'EmailConfirmation', component: EmailConfirmation },
+	{ path: '/confirmation', redirect: '/email-confirmation' }
 ]
 
 const router = createRouter({
