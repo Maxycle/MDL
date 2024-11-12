4a7805f943d9aad60992e78579d2a89c6b53fb82
Maxycle <maxycle@hotmail.com>
2024-11-12 12:17:58 +0100

    fix: use loadEnv

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,41 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: "../public",
-    emptyOutDir: true
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ],
+export default defineConfig(({ mode }) => {
+  // Load env variables based on mode
+  const env = loadEnv(mode, process.cwd(), '');
+
+  console.log('VITE_BACKEND_URL:', env.VITE_BACKEND_URL);
+
+  return {
+    plugins: [vue()],
+    build: {
+      outDir: "../public",
+      emptyOutDir: true,
+      rollupOptions: {
+        external: ['img/Flag_of_Anarcho-capitalism.png']
+      }
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  server: {
-		proxy: {
-			'/api': {
-				target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',  // Use import.meta.env instead of process.env
-				changeOrigin: true,
-			}
-		}
-	}	
-})
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
