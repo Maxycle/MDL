4d1e84886513de2248c1907af11d6be0b0b508b1
Maxycle <maxycle@hotmail.com>
2024-11-13 19:45:44 +0100

    refactor: avoid hardcoded url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,11 @@
 class UserMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts = {})
     @token = token
-    @confirmation_link = "http://libertarien.net/confirmation?confirmation_token=#{token}"
-    opts[:template_path] = 'devise/mailer' # Specify where to find the template
+    host = ActionMailer::Base.default_url_options[:host]
+    port = ActionMailer::Base.default_url_options[:port]
+    @confirmation_link = "http://#{host}:#{port}/confirmation?confirmation_token=#{token}"
+    
+    opts[:template_path] = 'devise/mailer'
     super
   end
 end
