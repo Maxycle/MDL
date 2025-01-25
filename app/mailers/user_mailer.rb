b8c2b5469523ba7868399c8017650399f9d80047
Maxycle <maxycle@hotmail.com>
2025-01-25 18:44:07 +0100

    fix: correct link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,14 @@ class UserMailer < Devise::Mailer
     @token = token
     host = ActionMailer::Base.default_url_options[:host]
     port = ActionMailer::Base.default_url_options[:port]
-    @confirmation_link = "http://#{host}:#{port}/confirmation?confirmation_token=#{token}"
     
+    # Construct the confirmation link without including the port if it's the default (80 for http or 443 for https)
+    if port.present? && ((port != 80 && port != 443) || host == 'localhost')
+      @confirmation_link = "http://#{host}:#{port}/confirmation?confirmation_token=#{token}"
+    else
+      @confirmation_link = "http://#{host}/confirmation?confirmation_token=#{token}"
+    end
+
     opts[:template_path] = 'devise/mailer'
     super
   end
