3afc3b3710cd500b5eaa50a60854c643bc2fa353
Maxycle <maxycle@hotmail.com>
2025-02-06 07:31:08 +0100

    refactor: clean up code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://libertarien.net', 'https://libertarien.net'
-		# origins '*'
+    # origins 'http://libertarien.net', 'https://libertarien.net'
+		origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      # credentials: false,
 			credentials: true,
       expose: ['access-token', 'expiry', 'token-type', 'Authorization']
   end
